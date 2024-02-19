d35ee2120622900873d477f90008243275714b09
nathan desmee <nathan.desmee.etu@univ-lille.fr>
2024-02-19 18:11:15 +0100

    rm class

--- WEB-INF/src/controleurs/restAPI.java
+++ WEB-INF/src/controleurs/restAPI.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public abstract class restAPI extends HttpServlet {
-    
+
     public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         if (req.getMethod().equalsIgnoreCase("PATCH")) {
             doPatch(req, res);
